839d0cf35cc11ccc161d4b3e9cc7a7ee14e2e77f
eltallerjsdev <64107536+eltallerjsdev@users.noreply.github.com>
2020-12-30 18:20:23 -0700

    Updates

---
+++ precache-manifest.27c716dd08aea86b36c616bdca3d4236.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab1fb3a300e3cf85e06976d523511aa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3b64aec1e156ed252c0d",
+    "url": "./static/css/2.87bd7d82.chunk.css"
+  },
+  {
+    "revision": "51c6743c2f56a3949ef4",
+    "url": "./static/css/main.b98a5216.chunk.css"
+  },
+  {
+    "revision": "3b64aec1e156ed252c0d",
+    "url": "./static/js/2.bbb65253.chunk.js"
+  },
+  {
+    "revision": "d759b06386f9bf608687dab3e74ad31e",
+    "url": "./static/js/2.bbb65253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c6743c2f56a3949ef4",
+    "url": "./static/js/main.57fa3779.chunk.js"
+  },
+  {
+    "revision": "a6ab7301b43c3fcfd888",
+    "url": "./static/js/runtime-main.954f794f.js"
+  }
+]);
